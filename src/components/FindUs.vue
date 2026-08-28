62715dfc6cafabd417eb7e399c437dd8c4253ebf
Sacieki <blackly.snoozer@gmail.com>
2026-08-28 16:21:19 +0700

    reverse code

--- src/components/FindUs.vue
+++ src/components/FindUs.vue
@@ -436,11 +436,10 @@ export default {
 
     loadGoogleMaps() {
       return new Promise((resolve, reject) => {
-        /*
+        
         const apiKey =
           process.env.VUE_APP_GOOGLE_MAPS_API_KEY
-        */
-       const apiKey = "AIzaSyCIc2yK9-LvMqk03jPZLKjr8qUA8HxJ1Kg"
+        
         console.log(
           'Google Maps Key exists:',
           !!apiKey
